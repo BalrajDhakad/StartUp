5a5e5cab169b223b0b4a9f70c6c477efde2d54e8
BalrajDhakad <dhakad.learner@gmail.com>
2024-03-18 05:08:43 -0400

    Added base files for the home page

---
+++ files/home/home.app.js
@@ -0,0 +1,9 @@
+let language = document.querySelector('.language');
+let languageDropdown = document.querySelector('.languageDropdown')
+
+let dropdownOn = 
+
+language.addEventListener('click', () => {
+    languageDropdown.style.display = 'none'
+    language.style.border
+})
