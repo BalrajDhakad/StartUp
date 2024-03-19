f67123da122a9325d1deed1332b079a339433b5f
BalrajDhakad <dhakad.learner@gmail.com>
2024-03-19 02:56:39 -0400

    Added new features to the navBar

--- files/home/home.app.js
+++ files/home/home.app.js
@@ -1,9 +1,20 @@
-let language = document.querySelector('.language');
-let languageDropdown = document.querySelector('.languageDropdown')
+// let language = document.querySelector('.language');
+// let languageDropdown = document.querySelector('.languageDropdown')
 
-let dropdownOn = 
+// let dropdownOn = "close";
 
-language.addEventListener('click', () => {
-    languageDropdown.style.display = 'none'
-    language.style.border
-})
+// language.addEventListener('click', () => {
+//     if (dropdownOn==="open") {
+//         languageDropdown.style.display = 'none'
+//         language.style.borderBottomLeftRadius = '10px'
+//         language.style.borderBottomRightRadius = '10px'
+//         dropdownOn = "close"
+//     }
+
+//     else if (dropdownOn === "close") {
+//         languageDropdown.style.display = 'block'
+//         language.style.borderBottomLeftRadius = '0'
+//         language.style.borderBottomRightRadius = '0'
+//         dropdownOn = "open"
+//     }
+// })
